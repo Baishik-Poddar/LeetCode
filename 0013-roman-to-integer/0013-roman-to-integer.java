2656431530d6f8ae15f710b6095f700120de0969
Baishik-Poddar <baishik145@gmail.com>
2026-05-04 02:22:52 +0530

    Time: 3 ms (78.82%), Space: 46.6 MB (51.62%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,49 +1,27 @@
-import java.util.Scanner;
-class Solution {
+class Solution 
+{
+    private static final Map<Character, Integer> roman = Map.of(
+        'I', 1,
+        'V', 5,
+        'X', 10,
+        'L', 50, 
+        'C', 100, 
+        'D', 500,
+        'M', 1000
+    );
     public int romanToInt(String s) 
     {
-      Scanner sc = new Scanner(System.in);
       int sum=0;
       boolean fg=false;
-      Map <Character, Integer> romandict = new HashMap<>();
-      romandict.put('I',1);
-      romandict.put('V',5);
-      romandict.put('X',10);
-      romandict.put('L',50);
-      romandict.put('C',100);
-      romandict.put('D',500);
-      romandict.put('M',1000);
-      for(int i=0; i<s.length();i++)
-      {
-        char schar = s.charAt(i);
-        int val = romandict.get(schar);
-        if(i<s.length()-1)
-        {
-            if(val < romandict.get(s.charAt(i+1)))
-            {
-                val= romandict.get(s.charAt(i+1))-val ;
-                // i++;
-                sum+=val;
-                fg=true;
-            }
-            else
-            {
-                if(fg==false)
-                    sum+=val;
-                else
-                {
-                    fg=false;
-                    continue;
-                }
-
-            }
+     for (int i = 0; i < s.length(); i++) 
+     {
+        int val = roman.get(s.charAt(i));
+        if (i < s.length() - 1 && val < roman.get(s.charAt(i + 1))) {
+            sum -= val;
+        } else {
+            sum += val;
         }
-        else
-        {
-            if(fg==false)
-                 sum+=val;
-        }
-    }
-      return sum;
+     }
+     return sum;
     }
 }
