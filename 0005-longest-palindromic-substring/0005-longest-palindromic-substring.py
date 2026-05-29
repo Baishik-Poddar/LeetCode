3499432452fa9b55e16263c043a3376b0608a1d9
Baishik-Poddar <baishik145@gmail.com>
2026-05-29 09:26:45 +0530

    Time: 112 ms (96.54%), Space: 19.3 MB (70.29%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,15 +1,12 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        longest_palindrome_start, longest_palindrome_len = 0, 1
-
+        start, longest_palindrome_len = 0, 1
         for i in range(0, n):
             right = i
             while right < n and s[i] == s[right]:
                 right += 1
-            # s[i, right - 1] inclusive are equal characters e.g. "aaa"
-            
-            # while s[left] == s[right], s[left, right] inclusive is palindrome e.g. "baaab"
+                
             left = i - 1
             while left >= 0 and right < n and s[left] == s[right]:
                 left -= 1
@@ -19,6 +16,6 @@ class Solution:
             palindrome_len = right - left - 1
             if palindrome_len > longest_palindrome_len:
                 longest_palindrome_len = palindrome_len
-                longest_palindrome_start = left + 1
+                start = left + 1
             
-        return s[longest_palindrome_start: longest_palindrome_start + longest_palindrome_len]
+        return s[start: start + longest_palindrome_len]
